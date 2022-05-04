e5e896e61bd79beb164a0686913870d979fd6c98
baki <badrkhatib@hotmail.com>
2022-05-04 15:33:42 +0200

    some changes + tests

--- client.c
+++ client.c
@@ -6,11 +6,54 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 18:12:35 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/04 15:05:06 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/04 15:32:59 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	stream_char(pid_t pid, char c)
+{
+	int	bit;
+	int	r;
+	
+	bit = 0;
+	r = 0;
+	cond = 1;
+	while (bit < CHAR_BIT && cond)
+	{
+		cond = 0;
+		if (c & 1)
+			r = kill(pid, SIGUSR1);
+		else
+			r = kill(pid, SIGUSR2);
+		if (r == -1)
+			ft_error("Invalid PID.\n");
+		if (!cond)
+			usleep(50);
+		c >>= 1;
+		usleep(50);
+		if (!cond)
+			ft_error("Signal failed to send.\n");
+		bit++;
+	}
+}
+
+void	stream_msg(pid_t pid, char *msg)
+{
+	while (*msg != '\0')
+	{
+		stream_char(pid_t pid, *msg);
+		msg++;
+	}
+	write(1, "String sent successfully. \n", 26);
+}
+
+void	handler(int sig)
+{
+	(void)sig;
+	cond = 1;
+}
 
 int	main(int argc, char **argv)
 {
@@ -24,4 +67,5 @@ int	main(int argc, char **argv)
 	}
 	else
 		write(1, "Usage: ./client [server PID] [message]\n", 39);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,13 +6,41 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 14:13:33 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/04 14:27:43 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/04 15:17:35 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	handler(int sig, siginfo_t *info, void *ucontext)
+{
+	static unsigned char	result;
+	static int				bit_position;
+	
+	(void)ucontext;
+	kill(info->si_pid, SIGUSR1);
+	result += (sig == SIGUSR1) << bit_position;
+	bit_position++;
+	if (bit_position > CHAR_BIT - 1)
+	{
+		if (result)
+			write(1, &result, 1);
+		bit_position = 0;
+		result = 0;
+	}
+}
+
 int	main(void)
 {
+	struct	sigaction	sa;
+	
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
 	ft_putnbr(getpid(), 10, "0123456789");
+	write(1, "\n", 1);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (19)
+		pause();
+	return(0);
 }
