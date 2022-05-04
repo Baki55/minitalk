cfd5ac8a9f045d94fe0fe9f35d8cce43e0367ec0
baki <badrkhatib@hotmail.com>
2022-05-04 15:15:26 +0200

    added client.c with handler

--- client.c
+++ client.c
@@ -6,15 +6,22 @@
 /*   By: bkhatib <bkhatib@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 18:12:35 by bkhatib           #+#    #+#             */
-/*   Updated: 2022/05/01 18:48:39 by bkhatib          ###   ########.fr       */
+/*   Updated: 2022/05/04 15:05:06 by bkhatib          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "minitalk.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	printf("PID: %d", getpid());
+	pid_t	serverpid;
+	
+	signal(SIGUSR1, handler);
+	if (argc == 3)
+	{
+		serverpid = ft_atoi(argc[1]);
+		ft_stream_str(argv[2], serverpid);
+	}
+	else
+		write(1, "Usage: ./client [server PID] [message]\n", 39);
 }
